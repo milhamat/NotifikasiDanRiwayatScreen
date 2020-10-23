3adde5f8e71121b46cd27148e4964969909a8a8e
MilhamAt <M477ashiddiq@gmail.com>
2020-10-23 21:22:57 +0700

    Initial Commit

---
+++ NotifikasiDanRiwayatScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotifikasiDanRiwayatScreen
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 23/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
